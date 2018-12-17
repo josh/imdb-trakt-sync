9cc3f80b2680bb72c542263f7c60e019127ce30e
Joshua Peek <josh@joshpeek.com>
2018-12-16 20:08:33 -0800

    Log nested result

--- start.js
+++ start.js
@@ -5,7 +5,7 @@ const interval = (parseInt(process.env["INTERVAL"]) || 3600) * 1000;
 setInterval(() => {
   handler()
     .then(result => {
-      console.info(`${new Date().toISOString()}:`, result);
+      console.info(`${new Date().toISOString()}:`, JSON.stringify(result));
     })
     .catch(error => {
       console.error(`${new Date().toISOString()}:`, error);
