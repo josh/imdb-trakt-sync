6d704d2bbda517101322560c0739363f4292139a
Joshua Peek <josh@joshpeek.com>
2018-12-18 18:25:43 -0800

    Trim whitespace

--- start.js
+++ start.js
@@ -5,7 +5,7 @@ const interval = (parseInt(process.env["INTERVAL"]) || 3600) * 1000;
 setInterval(async () => {
   try {
     const result = await handler();
-    console.info(`${new Date().toISOString()}:\n`, result);
+    console.info(`${new Date().toISOString()}:\n${result}`);
   } catch (error) {
     console.error(`${new Date().toISOString()}:`, error);
   }
