c461d57d9b343fe3f91213bb244f3de7fa6e2e1a
Joshua Peek <josh@joshpeek.com>
2018-12-18 16:23:37 -0800

    Use async/await

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const csv = require("csv");
 const fetch = require("node-fetch");
 
-function parseCSV(data) {
+async function parseCSV(data) {
   return new Promise((resolve, reject) => {
     csv.parse(data, (err, data) => {
       if (err) {
@@ -38,60 +38,60 @@ function isImdb(movie) {
   return movie.id.startsWith("tt");
 }
 
-function imdbWatchlist() {
-  return fetch(
+async function imdbWatchlist() {
+  const response = await fetch(
     `http://www.imdb.com/list/${process.env.IMDB_WATCHLIST_ID}/export`,
     {
       headers: {
         Cookie: `id=${process.env.IMDB_SESSION_ID}`
       }
     }
-  )
-    .then(response => response.text())
-    .then(text => parseCSV(text))
-    .then(rows => rows.map(row => ({ id: row[1] })).filter(isImdb));
+  );
+  const text = await response.text();
+  const rows = await parseCSV(text);
+  return rows.map(row => ({ id: row[1] })).filter(isImdb);
 }
 
-function imdbRatings() {
-  return fetch(
+async function imdbRatings() {
+  const response = await fetch(
     `http://www.imdb.com/user/${process.env.IMDB_RATINGS_ID}/ratings/export`,
     {
       headers: {
         Cookie: `id=${process.env.IMDB_ID}; sid=${process.env.IMDB_SID}`
       }
     }
-  )
-    .then(response => response.text())
-    .then(text => parseCSV(text))
-    .then(rows =>
-      rows
-        .map(row => ({
-          id: row[0],
-          rating: row[1],
-          timestamp: row[2]
-        }))
-        .filter(isImdb)
-        .map(({ id, rating, timestamp }) => ({
-          id,
-          rating: parseInt(rating),
-          timestamp: new Date(Date.parse(timestamp)).toISOString()
-        }))
-    );
+  );
+  const text = await response.text();
+  const rows = await parseCSV(text);
+
+  return rows
+    .map(row => ({
+      id: row[0],
+      rating: row[1],
+      timestamp: row[2]
+    }))
+    .filter(isImdb)
+    .map(({ id, rating, timestamp }) => ({
+      id,
+      rating: parseInt(rating),
+      timestamp: new Date(Date.parse(timestamp)).toISOString()
+    }));
 }
 
-function traktGet(path) {
-  return fetch(`https://api.trakt.tv${path}`, {
+async function traktGet(path) {
+  const response = await fetch(`https://api.trakt.tv${path}`, {
     headers: {
       Authorization: `Bearer ${process.env.TRAKT_ACCESS_TOKEN}`,
       "Content-Type": "application/json",
       "trakt-api-version": "2",
       "trakt-api-key": process.env.TRAKT_CLIENT_ID
     }
-  }).then(response => response.json());
+  });
+  return await response.json();
 }
 
-function traktPost(path, data) {
-  return fetch(`https://api.trakt.tv${path}`, {
+async function traktPost(path, data) {
+  const response = await fetch(`https://api.trakt.tv${path}`, {
     method: "POST",
     headers: {
       Accept: "application/json",
@@ -101,27 +101,25 @@ function traktPost(path, data) {
       "trakt-api-version": "2"
     },
     body: JSON.stringify(data)
-  }).then(response => response.json());
+  });
+  return await response.json();
 }
 
-function traktWatchlist() {
-  return traktGet("/sync/watchlist/movies").then(movies =>
-    movies.map(movie => ({ id: movie.movie.ids.imdb })).filter(isImdb)
-  );
+async function traktWatchlist() {
+  const movies = await traktGet("/sync/watchlist/movies");
+  return movies.map(movie => ({ id: movie.movie.ids.imdb })).filter(isImdb);
 }
 
-function traktRatings() {
-  return traktGet("/sync/ratings/movies").then(movies =>
-    movies
-      .map(movie => ({ id: movie.movie.ids.imdb, rating: movie.rating }))
-      .filter(isImdb)
-  );
+async function traktRatings() {
+  const movies = await traktGet("/sync/ratings/movies");
+  return movies
+    .map(movie => ({ id: movie.movie.ids.imdb, rating: movie.rating }))
+    .filter(isImdb);
 }
 
-function traktWatched() {
-  return traktGet("/sync/watched/movies").then(movies =>
-    movies.map(movie => ({ id: movie.movie.ids.imdb })).filter(isImdb)
-  );
+async function traktWatched() {
+  const movies = await traktGet("/sync/watched/movies");
+  return movies.map(movie => ({ id: movie.movie.ids.imdb })).filter(isImdb);
 }
 
 function traktAdd(movies) {
@@ -176,39 +174,37 @@ function traktUnwatch(movies) {
   });
 }
 
-function syncWatchlist() {
-  return Promise.all([imdbWatchlist(), traktWatchlist()]).then(
-    ([imdbWatchlist, traktWatchlist]) => {
-      const { add, remove } = diff(
-        imdbWatchlist,
-        traktWatchlist,
-        movie => movie.id
-      );
-      return Promise.all([traktAdd(add), traktRemove(remove)]);
-    }
+async function syncWatchlist() {
+  const imdbWatchlistPromise = imdbWatchlist();
+  const traktWatchlistPromise = traktWatchlist();
+  const { add, remove } = diff(
+    await imdbWatchlistPromise,
+    await traktWatchlistPromise,
+    movie => movie.id
   );
+  return await Promise.all([traktAdd(add), traktRemove(remove)]);
 }
 
-function syncRatings() {
-  return Promise.all([imdbRatings(), traktRatings()]).then(
-    ([imdbRatings, traktRatings]) => {
-      const { add, remove } = diff(
-        imdbRatings,
-        traktRatings,
-        movie => movie.id
-      );
-      return Promise.all([
-        traktRate(add),
-        traktWatch(add),
-        traktUnrate(remove),
-        traktUnwatch(remove)
-      ]);
-    }
+async function syncRatings() {
+  const imdbRatingsPromise = imdbRatings();
+  const traktRatingsPromise = traktRatings();
+  const { add, remove } = diff(
+    await imdbRatingsPromise,
+    await traktRatingsPromise,
+    movie => movie.id
   );
+  return await Promise.all([
+    traktRate(add),
+    traktWatch(add),
+    traktUnrate(remove),
+    traktUnwatch(remove)
+  ]);
 }
 
-function sync() {
-  return Promise.all([syncWatchlist(), syncRatings()]).then(summary);
+async function sync() {
+  const syncWatchlistPromise = syncWatchlist();
+  const syncRatingsPromise = syncRatings();
+  return summary(await syncWatchlistPromise, await syncRatingsPromise);
 }
 
 function changeSummary(result) {
@@ -232,9 +228,8 @@ function changeSummary(result) {
   return summary.length > 0 ? summary.join(", ") : "no change";
 }
 
-function summary(result) {
+function summary(watchlist, ratings) {
   try {
-    const [watchlist, ratings] = result;
     const [watchlistAdded, watchListDeleted] = watchlist;
     const [ratingsRate, ratingsWatch, ratingsUnrate, ratingsUnwatch] = ratings;
 
--- start.js
+++ start.js
@@ -2,12 +2,11 @@ const { handler } = require(".");
 
 const interval = (parseInt(process.env["INTERVAL"]) || 3600) * 1000;
 
-setInterval(() => {
-  handler()
-    .then(result => {
-      console.info(`${new Date().toISOString()}:`, result);
-    })
-    .catch(error => {
-      console.error(`${new Date().toISOString()}:`, error);
-    });
+setInterval(async () => {
+  try {
+    const result = await handler();
+    console.info(`${new Date().toISOString()}:\n`, result);
+  } catch (error) {
+    console.error(`${new Date().toISOString()}:`, error);
+  }
 }, interval);
