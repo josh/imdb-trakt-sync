ebb08c8192bcc7dd8d46452fe535643f5290b700
Joshua Peek <josh@joshpeek.com>
2019-03-21 23:23:25 -0700

    Convert imdb fetch to bash

--- main.sh
+++ main.sh
@@ -15,8 +15,30 @@ usage() {
 
 set -euxo pipefail
 
-node ./imdb-watchlist.js | tee imdb-watchlist.json
-node ./imdb-ratings.js | tee imdb-ratings.json
+imdb() {
+  curl --fail --silent --location \
+    --cookie "id=$IMDB_ID; sid=$IMDB_SID" \
+    "https://www.imdb.com$1"
+}
+
+imdb_watchlist() {
+  imdb "/list/$IMDB_WATCHLIST_ID/export" | \
+    ./node_modules/.bin/csvtojson | \
+    jq 'map({id: .Const})'
+}
+
+imdb_ratings() {
+  imdb "/user/$IMDB_RATINGS_ID/ratings/export" | \
+    ./node_modules/.bin/csvtojson | \
+    jq 'map({
+      id: .Const,
+      rating: .["Your Rating"] | tonumber,
+      timestamp: .["Date Rated"] | strptime("%Y-%m-%d") | todateiso8601
+    })'
+}
+
+imdb_watchlist > imdb-watchlist.json
+imdb_ratings > imdb-ratings.json
 
 node ./index.js watchlist imdb-watchlist.json
 node ./index.js ratings imdb-ratings.json
