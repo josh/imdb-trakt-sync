e5224d8c7ea4556c7480fb463a52891df61cf7d7
Joshua Peek <josh@joshpeek.com>
2019-03-21 22:29:26 -0700

    Add usage

--- main.sh
+++ main.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+# Usage: imdb-trakt-sync
+
+usage() {
+  sed -ne '/^#/!q;s/.\{1,2\}//;1d;p' < "$0"
+  exit 1
+}
+
+[ -n "$IMDB_ID" ] || usage
+[ -n "$IMDB_SID" ] || usage
+[ -n "$IMDB_RATINGS_ID" ] || usage
+[ -n "$IMDB_WATCHLIST_ID" ] || usage
+[ -n "$TRAKT_ACCESS_TOKEN" ] || usage
+[ -n "$TRAKT_CLIENT_ID" ] || usage
 
 set -ex
 
