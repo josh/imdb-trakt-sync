24bc96a18956a92376903aa287473167782e5b8c
Joshua Peek <josh@joshpeek.com>
2019-03-24 16:41:51 -0700

    Use process sub instead of tmp files

--- sync.sh
+++ sync.sh
@@ -15,16 +15,14 @@ usage() {
 
 set -euxo pipefail
 
-./imdb-watchlist.sh "$IMDB_WATCHLIST_ID" > imdb-watchlist.json
-./trakt-watchlist.sh "$TRAKT_CLIENT_ID" "$TRAKT_ACCESS_TOKEN" > trakt-watchlist.json
-node ./diff.js imdb-watchlist.json trakt-watchlist.json > watchlist-diff.json
-rm imdb-watchlist.json trakt-watchlist.json
-node ./index.js watchlist watchlist-diff.json
-rm watchlist-diff.json
+node ./index.js watchlist <(
+  node ./diff.js \
+    <(./imdb-watchlist.sh "$IMDB_WATCHLIST_ID") \
+    <(./trakt-watchlist.sh "$TRAKT_CLIENT_ID" "$TRAKT_ACCESS_TOKEN")
+)
 
-./imdb-ratings.sh "$IMDB_RATINGS_ID" "$IMDB_ID" "$IMDB_SID" > imdb-ratings.json
-./trakt-ratings.sh "$TRAKT_CLIENT_ID" "$TRAKT_ACCESS_TOKEN" > trakt-ratings.json
-node ./diff.js imdb-ratings.json trakt-ratings.json > ratings-diff.json
-rm imdb-ratings.json trakt-ratings.json
-node ./index.js ratings ratings-diff.json
-rm ratings-diff.json
+node ./index.js ratings <(
+  node ./diff.js \
+    <(./imdb-ratings.sh "$IMDB_RATINGS_ID" "$IMDB_ID" "$IMDB_SID") \
+    <(./trakt-ratings.sh "$TRAKT_CLIENT_ID" "$TRAKT_ACCESS_TOKEN")
+)
