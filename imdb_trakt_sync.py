adbbe038b0ee4445f93e29edeac70f003aed15b3
Joshua Peek <josh@users.noreply.github.com>
2024-07-08 23:43:07 +0000

    Define IMDB_WATCHLIST_URL config var

--- imdb_trakt_sync.py
+++ imdb_trakt_sync.py
@@ -2,8 +2,13 @@ import click
 
 
 @click.command()
-def main() -> None:
-    click.echo("Hello, World!")
+@click.option(
+    "--imdb-watchlist-url",
+    required=True,
+    envvar="IMDB_WATCHLIST_URL",
+)
+def main(imdb_watchlist_url: str) -> None:
+    click.echo(imdb_watchlist_url)
 
 
 if __name__ == "__main__":
